364a89b81821820fd232beff8ee2919c17ba96dc
gightkingss <gightkingss@gmail.com>
2024-06-27 20:33:16 +0300

    GCL

---
+++ src/views/HomePage/FeaturesSection.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="px-4 flex justify-around">
+    <div  v-for="(feature,i) in features" :key="i"  class=" 
+     h-[400px] w-1/4 rounded-[50px]   py-5 px-2 animate__animated animate__rubberBand">
+        <VEmptyState class="hover:animate-pulse focus:animate-spin   rounded-2xl  font-bold " style="font-size: 40px;"
+            size="100"
+            height="100"
+            :class="{ 'bg-gradient-to-b from-blue-500 to-orange-600': i % 2 === 0, 'bg-gradient-to-b from-orange-600 to-blue-500': i % 2!== 0 }"
+            :title=feature.title
+            :icon=feature.icon
+            :text=feature.text
+        >{{ feature.headline  }}</VEmptyState>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import 'animate.css';
+// import IconCommunityVue from "@/components/icons/IconFast.vue";
+import IconFastVue from "@/components/icons/IconFast.vue";
+import IconReliable from "@/components/icons/IconReliable.vue";
+import IconPrice from "@/components/icons/IconPrice.vue";
+import IconSupport from "@/components/icons/IconSupport.vue";
+import { VEmptyState} from "vuetify/lib/components/index.mjs";
+
+const features = [
+    {
+        icon: IconFastVue,
+        headline:"FAST",
+        title:"Best in town",
+        text:"Constant connectivty flow at any time. Forget about downtimes!!",
+    },
+    {
+        icon: IconReliable,
+        headline:"RELIABLE",
+        title:"We got this handled",
+        text:"We let you worry on key parts of your productivity.We handle the rest.",
+    },
+    {
+        icon: IconPrice,
+        headline:"AFFORDABLE",
+        title:"Plans that fit you",
+        text:"Talking about quality at a cheap price...we are at the forefront.",
+    },
+    {
+        icon: IconSupport,
+        headline:"SUPPORT",
+        title:"Around the clock support",
+        text:"Support team ready and available always to cater for your need as a client.",
+    },
+]
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomePage/HeroSection.vue
@@ -0,0 +1,103 @@
+<template>
+  <!-- <div class="w-full h-[90vh] bg-slate-400 overflow-hidden">
+    <img src="/Images/gigabit-happy-family.jpg" class="w-full" alt="">
+  </div> -->
+
+  <v-carousel  
+    v-if="plans"
+    
+    height="600"
+    style="width: 100vw;"
+    class="border mx-auto"
+    show-arrows="hover"
+    cycle
+    interval="5000"
+    hide-delimiter-background
+  >
+    <v-carousel-item
+      v-for="(plan, i) in plans.length-1"
+      :key="i"
+    >
+      <v-sheet
+        class=""
+        height="100%"
+      >
+        <div class="d-flex fill-height justify-center align-center w-screen animate__animated animate__jackInTheBox">
+            
+            <!-- <div class="text-h2">
+            {{ plan-1 }}
+          </div> -->
+          <img :src=plans[plan-1].imagePath class="w-full" alt="">
+          <div class="w-[500px] bg-cyan-500 rounded absolute text-center  h-[200px] opacity-90  mt-[150px] -ml-[700px] ">
+            <p class="text-5xl text-center py-5 font-bold text-white">{{plans[plan-1].name}}</p>
+            <p>{{plans[plan-1].description}}</p>
+            <p>Pocket friendly from as low as <span class="text-xl font-bold">KSH {{plans[plan-1].price}} bob</span> </p>
+            <div>
+                <div class="px-5 font-bold bg-orange-400 w-fit rounded-md py-3 text-xl mx-auto">GET THE PLAN</div>
+
+            </div>
+          </div>
+        </div>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script setup>
+import 'animate.css'
+import { VCarousel } from 'vuetify/lib/components/index.mjs'
+const plans = [
+    {
+        name:'HOME FIBRE',
+        description: 'Dont miss a moment',
+        price:'2199',
+        imagePath:"/Images/gigabit-happy-family.jpg",
+        direction:"horizontal"
+    },
+    {
+        name:'HOME FIBRE',
+        description: 'Happy moments tailored for you',
+        price:'2199',
+        imagePath:"/Images/gigabit-happy-family.jpg",
+        direction:"horizontal"
+
+    },
+    {
+        name:'BUSINESS FIBRE',
+        description: 'Keeping you up with no ease ',
+        price:'2199',
+        imagePath:"/public/Images/gigabit-bussiness-talks.jpg",
+        direction:"horizontal"
+
+    },
+    {
+        name:'BUSINESS FIBRE',
+        description:'Focus on the profit we take care of the connectivity',
+
+        price:'2199',
+        imagePath:"/public/Images/gigabit-bussiness.jpeg",
+        direction:"horizontal"
+
+    },
+    {
+        name:'STUDENT PLANS',
+        description: 'Improve Grade performance with ease',
+        price:'999',
+        imagePath:"/Images/gigabit-happy-family.jpg",
+        direction:"horizontal"
+
+    },
+    {
+        name:'STUDENT PLANS',
+        description: 'When it comes to taking a break we got you!',
+        price:'1500',
+        imagePath:"/Images/gigabit-happy-family.jpg",
+        direction:"horizontal"
+
+    },
+]
+</script>
+
+<style>
+
+</style>
