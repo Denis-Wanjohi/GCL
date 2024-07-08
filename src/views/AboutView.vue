ef1f173cb9b3e4937368e7eb0fd78b59148ac80f
guanl20 <158102824+guanl20@users.noreply.github.com>
2024-07-08 11:26:47 +0300

    Create AboutView.vue

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="space-y-4">
+    <v-card image="https://i.pinimg.com/736x/41/8e/bc/418ebcab739281b2410d28f2349ff554.jpg">
+      <WhoWeAreSection></WhoWeAreSection>
+    </v-card>
+
+    <v-card image="https://i.pinimg.com/564x/72/2b/da/722bdace96d8290d7dd8c99a381adad6.jpg">
+      <WhyChooseUsSection></WhyChooseUsSection>
+      <WhyUsSection></WhyUsSection>
+    </v-card>
+
+    <MeetUsSection></MeetUsSection>
+  </div>
+</template>
+
+<script setup>
+import WhoWeAreSection from "./HomePage/WhoWeAre.vue";
+import WhyChooseUsSection from "./HomePage/WhyChooseUs.vue";
+import WhyUsSection from "./HomePage/WhyUs.vue";
+import MeetUsSection from "./HomePage/MeetUs.vue"; // Corrected component name
+</script>
+
+<style>
+/* Your styles here */
+</style>
