2ddf9558b3b5ed8663c0a19da78cbf64121c5d26
guanl20 <158102824+guanl20@users.noreply.github.com>
2024-07-08 11:34:10 +0300

    Create WhoWeAre.vue

---
+++ src/views/HomePage/WhoWeAre.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="flex py-20">
+<img class="p-8 w-[400px] h-[400px] img"
+src='https://i.pinimg.com/736x/31/5f/ac/315facab69e96dd7a9d18c7f212bf1e9.jpg' alt="wifi image" >
+    <div class="justify align-center px-30">
+       <div class="underline justify-end text-3xl text center w-full pl-100 px-20">WHO ARE WE?</div>
+       <p class="flex align-center text-center px-20 font-sans"> 
+       Founded in the midst of the COVID-19 pandemic, Gigabit was born out of a desire to keep people connected and 
+       productive during a time of unprecedented isolation. Our founders, a team of passionate entrepreneurs and 
+       tech enthusiasts, saw an opportunity to revolutionize the way we access the internet and stay connected with 
+       loved ones. With a focus on speed, reliability, and security, Gigabit quickly gained traction as a trusted 
+       provider of high-speed internet solutions.
+       </p> 
+    </div>
+</div>  
+</template>
+<script setup>
+</script>
+  <style></style>
