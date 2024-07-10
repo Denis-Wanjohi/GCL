9f4d56067def6a3555b6b423d6b645d0e450544b
gightkingss <gightkingss@gmail.com>
2024-07-10 11:15:12 +0300

    GCL

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -85,6 +85,13 @@
                             </div>
 
                             <div class="sm:col-span-3">
+                            <label for="whatsAppNumber" class="block text-sm font-semibold leading-6 text-gray-900">WhatsApp number</label>
+                            <div class="mt-2">
+                                <input id="whatsAppNumber" required   v-model="details.whatsAppNumber" name="whatsAppNumber" type="tel" v-maska  data-maska="+254#########" placeholder="+2547XXXXXXXX" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3">
+                            </div>
+                            </div>
+
+                            <div class="sm:col-span-3">
                                 <label for="location" class=" block text-sm font-semibold leading-6 text-gray-900">Location</label>
                                 <div class="mt-2">
                                     <select id="location" required  v-model="details.location" name="location"  class="block px-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
@@ -213,6 +220,7 @@ const details = ref({
     lastname:'',
     email:'',
     phoneNumber:'',
+    whatsAppNumber:'',
     location:'',
     service:'',
     message:'',
@@ -238,6 +246,7 @@ function userDetails(){
             lastName: details.value.lastname,
             email: details.value.email,
             phoneNumber: details.value.phoneNumber,
+            whatsAppNumber: details.value.whatsAppNumber,
             location: details.value.location,
             service: details.value.service,
     }
@@ -267,6 +276,7 @@ function userDetails(){
             details.value.lastname =''
             details.value.email =''
             details.value.phoneNumber = ''
+            details.value.whatsAppNumber = ''
             details.value.location = ''
             details.value.service = ''
             details.value.message = ''
