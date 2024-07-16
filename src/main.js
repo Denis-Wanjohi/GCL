139ad1ce2655fa74f3d8818bdbbc206f6c4a1c4d
gightkingss <gightkingss@gmail.com>
2024-07-16 13:03:10 +0300

    GCL

--- src/main.js
+++ src/main.js
@@ -9,19 +9,19 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import App from './App.vue'
 import router from './router'
-// import TawkMessengerVue from '@tawk.to/tawk-messenger-vue-3';
+import TawkMessengerVue from '@tawk.to/tawk-messenger-vue-3';
 const vuetify = createVuetify({
     components,
     directives,
 })
 const app = createApp(App)
-// app.use(TawkMessengerVue, {
+app.use(TawkMessengerVue, {
 
-//     propertyId : '6690192cc3fb85929e3e3280',
+    propertyId : '6690192cc3fb85929e3e3280',
   
-//     widgetId : '1i2he4mi8'
+    widgetId : '1i2he4mi8'
   
-//   });
+  });
 app.use(router)
 app.use(vuetify)
 app.mount('#app')
