34c1b1b3b4f7c418613cad0df24f39ee210ef635
gightkingss <gightkingss@gmail.com>
2024-07-20 12:36:22 +0300

    GCL

--- src/App.vue
+++ src/App.vue
@@ -156,14 +156,16 @@ function supportReq(){
       </div>
 
     </header>
-    <div class=" px-2  inset-0 z-10 fixed top-3/4 flex text-center justify-center align-center">
+    <div class=" px-2  inset-0 z-10 fixed top-3/4  text-center w-20 h-20 ">
       <!-- <v-fab extended prepend-icon="$vuetify" variant="tonal" class="text-blue-500" text="Reach out"></v-fab> -->
-      <v-fab @click="supportReq">
+      <v-fab @click="supportReq" absolute location="center">
         <svg xmlns="http://www.w3.org/2000/svg" class="text-blue-700" width="20px" height="20px" viewBox="0 0 256 256">
           <rect width="256" height="256" fill="none" />
           <path fill="currentColor" d="M64 64a48 48 0 0 1 96 0a8 8 0 0 1-16 0a32 32 0 0 0-64 0a8 8 0 0 1-16 0m143.23 56c-8.61.4-15.23 7.82-15.23 16.43v7.28a8.17 8.17 0 0 1-7.47 8.25a8 8 0 0 1-8.53-8v-23.51c0-8.61-6.62-16-15.23-16.43A16 16 0 0 0 144 120v15.73a8.17 8.17 0 0 1-7.47 8.25a8 8 0 0 1-8.53-8V64.45c0-8.61-6.62-16-15.23-16.43A16 16 0 0 0 96 64v119.74a8.19 8.19 0 0 1-6.72 8.16h-.12a6.09 6.09 0 0 1-6-3.09l-21-36.44c-4.3-7.46-13.74-10.57-21.4-6.62A16 16 0 0 0 34.15 168l30.95 60.05A8 8 0 0 0 72 232h136a8 8 0 0 0 7.16-4.42c.36-.72 8.84-15.06 8.84-40.65V136a16 16 0 0 0-16.77-16" />
         </svg>
+        
       </v-fab>
+      <!-- <p class="absolute top-10 ">Talk to us!</p> -->
     </div>
     <RouterView />
     <footer>
