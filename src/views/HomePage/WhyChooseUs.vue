9e2cf0efc2d7c4cbb62c5f3e051577778f075ec4
guanl20 <158102824+guanl20@users.noreply.github.com>
2024-07-24 11:39:11 +0300

    Update WhyChooseUs.vue

--- src/views/HomePage/WhyChooseUs.vue
+++ src/views/HomePage/WhyChooseUs.vue
@@ -3,7 +3,8 @@
 <div class="sm:flex sm:h-[50vh] mb-10">   
   <img class="sm:w-1/2" src="/Images/GCL_main_logo.png" alt="">
   <div class=" sm:w-1/2 text-md font-semibold   flex justify-center align-center">
-    Choose us for a seamless internet experience! Our ultra-fast fibre optic connection ensures uninterrupted browsing, streaming, and downloading. <br> But what sets us apart is our customer-centric support. <br> Our dedicated team is always available to address your concerns, providing personalized assistance to ensure your needs are met. <br> And the best part? Our affordable plans make it easy on your wallet. <br> With us, you get the perfect blend of speed, support, and savings. <br> Experience the difference for yourself!
+    Choose us for a seamless internet experience! Our ultra-fast fibre optic connection ensures uninterrupted browsing, streaming, and downloading. <br> However, our focus on customer service is what makes us unique.
+ <br> Our dedicated team is always available to address your concerns, providing personalized assistance to ensure your needs are met. <br> Moreover, our cost-effective plans ease the financial burden. <br> With us, you get the perfect blend of speed, support, and savings. <br>Take a look and experience the difference with us today!
   </div>
 </div> 
 </template>
