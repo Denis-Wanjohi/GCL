df4080db3a3721e2e41195130fa43872d6daf602
gightkingss <gightkingss@gmail.com>
2024-07-30 14:44:55 +0300

    GCL

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,10 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Origin', 'https://gcl-jxde.vercel.app');
+  res.header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
